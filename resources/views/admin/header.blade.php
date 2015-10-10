f0145cbe94001eece9c33f077d3283a087e6bc5d
suwanji <suwanji@gmail.com>
2015-10-10 18:42:37 +0530

    tests in admin lte left panel

--- resources/views/admin/header.blade.php
+++ resources/views/admin/header.blade.php
@@ -1,6 +1,5 @@
 <!-- Main Header -->
 <header class="main-header">
-
   <!-- Logo -->
   <a href="index2.html" class="logo">
     <!-- mini logo for sidebar mini 50x50 pixels -->
@@ -8,19 +7,16 @@
     <!-- logo for regular state and mobile devices -->
     <span class="logo-lg"><b>Admin</b>LTE</span>
   </a>
-
-  <!-- Header Navbar -->
+  <!-- Header Navbar: style can be found in header.less -->
   <nav class="navbar navbar-static-top" role="navigation">
     <!-- Sidebar toggle button-->
     <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
       <span class="sr-only">Toggle navigation</span>
     </a>
-    <!-- Navbar Right Menu -->
     <div class="navbar-custom-menu">
       <ul class="nav navbar-nav">
         <!-- Messages: style can be found in dropdown.less-->
         <li class="dropdown messages-menu">
-          <!-- Menu toggle button -->
           <a href="#" class="dropdown-toggle" data-toggle="dropdown">
             <i class="fa fa-envelope-o"></i>
             <span class="label label-success">4</span>
@@ -28,32 +24,75 @@
           <ul class="dropdown-menu">
             <li class="header">You have 4 messages</li>
             <li>
-              <!-- inner menu: contains the messages -->
+              <!-- inner menu: contains the actual data -->
               <ul class="menu">
                 <li><!-- start message -->
                   <a href="#">
                     <div class="pull-left">
-                      <!-- User Image -->
-                      <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                      <img src="{{ asset("/bower_components/admin-lte/dist/img/user2-160x160.jpg")}}" class="img-circle" alt="User Image">
                     </div>
-                    <!-- Message title and timestamp -->
                     <h4>
                       Support Team
                       <small><i class="fa fa-clock-o"></i> 5 mins</small>
                     </h4>
-                    <!-- The message -->
                     <p>Why not buy a new awesome theme?</p>
                   </a>
                 </li><!-- end message -->
-              </ul><!-- /.menu -->
+                <li>
+                  <a href="#">
+                    <div class="pull-left">
+                      <img src="{{ asset("/bower_components/admin-lte/dist/img/user3-128x128.jpg")}}" class="img-circle" alt="User Image">
+                    </div>
+                    <h4>
+                      AdminLTE Design Team
+                      <small><i class="fa fa-clock-o"></i> 2 hours</small>
+                    </h4>
+                    <p>Why not buy a new awesome theme?</p>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <div class="pull-left">
+                      <img src="{{ asset("/bower_components/admin-lte/dist/img/user4-128x128.jpg")}}" class="img-circle" alt="User Image">
+                    </div>
+                    <h4>
+                      Developers
+                      <small><i class="fa fa-clock-o"></i> Today</small>
+                    </h4>
+                    <p>Why not buy a new awesome theme?</p>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <div class="pull-left">
+                      <img src="{{ asset("/bower_components/admin-lte/dist/img/user3-128x128.jpg")}}" class="img-circle" alt="User Image">
+                    </div>
+                    <h4>
+                      Sales Department
+                      <small><i class="fa fa-clock-o"></i> Yesterday</small>
+                    </h4>
+                    <p>Why not buy a new awesome theme?</p>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <div class="pull-left">
+                      <img src="{{ asset("/bower_components/admin-lte/dist/img/user4-128x128.jpg")}}" class="img-circle" alt="User Image">
+                    </div>
+                    <h4>
+                      Reviewers
+                      <small><i class="fa fa-clock-o"></i> 2 days</small>
+                    </h4>
+                    <p>Why not buy a new awesome theme?</p>
+                  </a>
+                </li>
+              </ul>
             </li>
             <li class="footer"><a href="#">See All Messages</a></li>
           </ul>
-        </li><!-- /.messages-menu -->
-
-        <!-- Notifications Menu -->
+        </li>
+        <!-- Notifications: style can be found in dropdown.less -->
         <li class="dropdown notifications-menu">
-          <!-- Menu toggle button -->
           <a href="#" class="dropdown-toggle" data-toggle="dropdown">
             <i class="fa fa-bell-o"></i>
             <span class="label label-warning">10</span>
@@ -61,21 +100,40 @@
           <ul class="dropdown-menu">
             <li class="header">You have 10 notifications</li>
             <li>
-              <!-- Inner Menu: contains the notifications -->
+              <!-- inner menu: contains the actual data -->
               <ul class="menu">
-                <li><!-- start notification -->
+                <li>
                   <a href="#">
                     <i class="fa fa-users text-aqua"></i> 5 new members joined today
                   </a>
-                </li><!-- end notification -->
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-warning text-yellow"></i> Very long description here that may not fit into the page and may cause design problems
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-users text-red"></i> 5 new members joined
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-shopping-cart text-green"></i> 25 sales made
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-user text-red"></i> You changed your username
+                  </a>
+                </li>
               </ul>
             </li>
             <li class="footer"><a href="#">View all</a></li>
           </ul>
         </li>
-        <!-- Tasks Menu -->
+        <!-- Tasks: style can be found in dropdown.less -->
         <li class="dropdown tasks-menu">
-          <!-- Menu Toggle Button -->
           <a href="#" class="dropdown-toggle" data-toggle="dropdown">
             <i class="fa fa-flag-o"></i>
             <span class="label label-danger">9</span>
@@ -83,20 +141,56 @@
           <ul class="dropdown-menu">
             <li class="header">You have 9 tasks</li>
             <li>
-              <!-- Inner menu: contains the tasks -->
+              <!-- inner menu: contains the actual data -->
               <ul class="menu">
                 <li><!-- Task item -->
                   <a href="#">
-                    <!-- Task title and progress text -->
                     <h3>
                       Design some buttons
                       <small class="pull-right">20%</small>
                     </h3>
-                    <!-- The progress bar -->
                     <div class="progress xs">
-                      <!-- Change the css width attribute to simulate progress -->
                       <div class="progress-bar progress-bar-aqua" style="width: 20%" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
                         <span class="sr-only">20% Complete</span>
+                      </div>
+                    </div>
+                  </a>
+                </li><!-- end task item -->
+                <li><!-- Task item -->
+                  <a href="#">
+                    <h3>
+                      Create a nice theme
+                      <small class="pull-right">40%</small>
+                    </h3>
+                    <div class="progress xs">
+                      <div class="progress-bar progress-bar-green" style="width: 40%" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
+                        <span class="sr-only">40% Complete</span>
+                      </div>
+                    </div>
+                  </a>
+                </li><!-- end task item -->
+                <li><!-- Task item -->
+                  <a href="#">
+                    <h3>
+                      Some task I need to do
+                      <small class="pull-right">60%</small>
+                    </h3>
+                    <div class="progress xs">
+                      <div class="progress-bar progress-bar-red" style="width: 60%" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
+                        <span class="sr-only">60% Complete</span>
+                      </div>
+                    </div>
+                  </a>
+                </li><!-- end task item -->
+                <li><!-- Task item -->
+                  <a href="#">
+                    <h3>
+                      Make beautiful transitions
+                      <small class="pull-right">80%</small>
+                    </h3>
+                    <div class="progress xs">
+                      <div class="progress-bar progress-bar-yellow" style="width: 80%" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
+                        <span class="sr-only">80% Complete</span>
                       </div>
                     </div>
                   </a>
@@ -108,19 +202,16 @@
             </li>
           </ul>
         </li>
-        <!-- User Account Menu -->
+        <!-- User Account: style can be found in dropdown.less -->
         <li class="dropdown user user-menu">
-          <!-- Menu Toggle Button -->
           <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-            <!-- The user image in the navbar-->
-            <img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-            <!-- hidden-xs hides the username on small devices so only the image appears. -->
+            <img src="{{ asset("/bower_components/admin-lte/dist/img/user2-160x160.jpg")}}" class="user-image" alt="User Image">
             <span class="hidden-xs">Alexander Pierce</span>
           </a>
           <ul class="dropdown-menu">
-            <!-- The user image in the menu -->
+            <!-- User image -->
             <li class="user-header">
-              <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+              <img src="{{ asset("/bower_components/admin-lte/dist/img/user2-160x160.jpg")}}" class="img-circle" alt="User Image">
               <p>
                 Alexander Pierce - Web Developer
                 <small>Member since Nov. 2012</small>
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -31,26 +31,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 </head>
-<!--
-BODY TAG OPTIONS:
-=================
-Apply one or more of the following classes to get the
-desired effect
-|---------------------------------------------------------|
-| SKINS         | skin-blue                               |
-|               | skin-black                              |
-|               | skin-purple                             |
-|               | skin-yellow                             |
-|               | skin-red                                |
-|               | skin-green                              |
-|---------------------------------------------------------|
-|LAYOUT OPTIONS | fixed                                   |
-|               | layout-boxed                            |
-|               | layout-top-nav                          |
-|               | sidebar-collapse                        |
-|               | sidebar-mini                            |
-|---------------------------------------------------------|
--->
+
 <body class="skin-blue">
 <div class="wrapper">
 
@@ -63,23 +44,8 @@ desired effect
             <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
         <!-- Content Header (Page header) -->
-        <section class="content-header">
-            <h1>
-                {{ $page_title or "Page Title" }}
-                <small>{{ $page_description or null }}</small>
-            </h1>
-            <!-- You can dynamically generate breadcrumbs here -->
-            <ol class="breadcrumb">
-                <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-                <li class="active">Here</li>
-            </ol>
-        </section>
 
-        <!-- Main content -->
-        <section class="content">
-            <!-- Your Page Content Here -->
-            @yield('content')
-        </section><!-- /.content -->
+        <!-- /.content -->
     </div><!-- /.content-wrapper -->
 
     <!-- Footer -->
@@ -95,6 +61,11 @@ desired effect
 <script src="{{ asset("/bower_components/admin-lte/bootstrap/js/bootstrap.min.js") }}"></script>
 <!-- AdminLTE App -->
 <script src="{{ asset("/bower_components/admin-lte/dist/js/app.min.js") }}"></script>
+<!-- Slimscroll -->
+<script src="{{ asset("/bower_components/admin-lte/plugins/slimScroll/jquery.slimscroll.min.js") }}"></script>
+<!-- FastClick -->
+<script src="{{ asset("/bower_components/admin-lte/plugins/fastclick/fastclick.min.js") }}"></script>
+<!-- AdminLTE App -->
 
 <!-- Optionally, you can add Slimscroll and FastClick plugins.
      Both of these plugins are recommended to enhance the
